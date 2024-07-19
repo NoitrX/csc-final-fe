78d32962df36f0dcaa0b64f2f82a968542e01484
Mednoob <ahmadyaqdhan123@gmail.com>
2024-07-19 22:23:57 +0900

    feat(Navbar): add join us button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { FiPhone } from "react-icons/fi";
+
 export default function Navbar() {
     return (
         <nav className="w-full flex items-center justify-between font-ubuntu font-semibold text-base py-5 px-10 border-b border-black uppercase">
@@ -9,6 +11,10 @@ export default function Navbar() {
                 <p>Event</p>
                 <p>Faq</p>
                 <p>Contact</p>
+                <button className="bg-primary text-white py-2 px-10 flex items-center gap-3">
+                    <FiPhone />
+                    <p>Join Us</p>
+                </button>
             </div>
         </nav>
     )
