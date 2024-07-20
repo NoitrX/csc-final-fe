015eb33d6e6e3a2934e76280984fe382918bc4e4
Mednoob <ahmadyaqdhan123@gmail.com>
2024-07-20 16:54:00 +0900

    feat: replace with button element

--- src/components/FAQQuestion.tsx
+++ src/components/FAQQuestion.tsx
@@ -6,12 +6,12 @@ export default function FAQQuestion({ question, answer }: { question: string; an
 
     return (
         <div className="w-full flex flex-col text-white">
-            <div className="w-full bg-[#0B101C] rounded-md py-2 pl-14 pr-5 flex justify-between gap-3 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
+            <button className="w-full bg-[#0B101C] rounded-md py-2 pl-14 pr-5 flex justify-between gap-3 cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
                 <p className="font-poppins font-bold">{question}</p>
                 <div className="bg-gradient-to-br from-[#01124F] to-white min-w-7 max-w-7 h-7 flex justify-center items-center rounded-md">
                     <FaChevronDown className={isOpen === null ? "" : isOpen ? "animate-faq-chevron-rotate" : "animate-faq-chevron-unrotate"}/>
                 </div>
-            </div>
+            </button>
             <div className={`w-full px-10 max-h-0 py-0 overflow-hidden ${isOpen === null ? "" : isOpen ? "animate-faq-appear" : "animate-faq-disappear"} text-[#B8B8B8]`}>
                 {answer}
             </div>
