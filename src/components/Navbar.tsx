5044634f0e09ad61430efa85b2fbe7d050be0c72
Mednoob <ahmadyaqdhan123@gmail.com>
2024-07-20 16:46:05 +0900

    feat(Navbar): responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="w-full flex items-center justify-between font-ubuntu font-semibold text-base py-5 px-10 border-b border-black uppercase">
             <p>Logo</p>
-            <div className="hidden md:flex items-center gap-10">
+            <div className="hidden md:flex items-center gap-3 lg:gap-10">
                 <Link href="/">Home</Link>
                 <Link href="/about">About</Link>
                 <p>Division</p>
