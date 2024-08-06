3add3e9dbd974694f180adc11916e16aa5d40619
Naufhal <naufhalsimdigxrpl2@gmail.com>
2024-08-06 15:02:14 +0700

    FAQ CMS

---
+++ src/app/admin/dashboard/faq/add/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+import InputLabel from "@/components/InputLabel";
+import XButton from "@/components/XButton";
+import { showSuccessAlert } from "@/utils/alert";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+export default function AddFaq() {
+  const router = useRouter();
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+  });
+
+  const [alert, setAlert] = useState({
+    status: false,
+    type: "",
+    msg: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  console.log(form);
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:9000/api/csc/faq", form);
+      showSuccessAlert("Data Faq Has Been Added Successfully!");
+      router.push("/admin/dashboard/faq");
+    } catch (err: any) {
+      console.log(err);
+      setAlert({
+        status: true,
+        type: "error",
+        msg: err.response.data.msg,
+      });
+    }
+  };
+  return (
+    <div className=" max-w-screen">
+      <div className="border border-gray-200 p-6 mt-3 rounded-md w-full">
+        <h2 className="font-semibold">Form Faq</h2>
+        <p className="text-red-500">{alert.msg}</p>
+        <hr className="mt-2" />
+        <form className="w-full">
+          <div className="mt-3 flex w-full justify-between gap-3">
+            <InputLabel errors={alert.msg} label="Title" type="text" placeholder="Enter Title" name="title" value={form.title} onChange={handleChange} />
+          </div>
+
+          <div className="mt-3 flex w-full justify-between gap-3">
+            <InputLabel errors={alert.msg} label="Description" type="text" placeholder="Enter Description" name="description" value={form.description} onChange={handleChange} />
+          </div>
+
+          <div className="flex justify-end items-end mt-3">
+            <XButton onClick={handleSubmit}>SUBMIT</XButton>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/app/admin/dashboard/faq/page.tsx
+++ src/app/admin/dashboard/faq/page.tsx
@@ -1,7 +1,85 @@
-import React from "react";
+"use client";
+import Thead from "@/components/Thead";
+import { FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { showConfirmDeleteAlert } from "@/utils/alert";
 
 function FaqPage() {
-  return <div>FaqPage</div>;
+  const [data, setData] = useState<FaqData[]>([]);
+
+  interface FaqData {
+    id: number;
+    title: string;
+    description: string;
+  }
+
+  const getAboutData = () => {
+    try {
+      fetch(`http://localhost:9000/api/csc/faq`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setData(data.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async (id: number) => {
+    try {
+      await showConfirmDeleteAlert(`http://localhost:9000/api/csc/faq/delete/${id}`);
+      await getAboutData();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getAboutData();
+  }, []);
+  return (
+    <div>
+      <div className="flex flex-col mt-6 text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl max-w-screen">
+        <div className="p-6">
+          <div className="flex flex-col justify-end items-end">
+            <button className="flex items-center py-2 px-16 bg-primary mb-4 text-white font-bold gap-5 font-ubuntu hover:bg-blue-700 transition-all duration-200">
+              <Link href={"/admin/dashboard/faq/add"} className="uppercase">
+                Tambah DATA
+              </Link>
+            </button>
+          </div>
+
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white border border-gray-200">
+              <Thead theads={["#", "Title", "Description"]} />
+              <tbody className="text-xs">
+                {data.map((d: FaqData, i) => {
+                  return (
+                    <tr className="text-center" key={i}>
+                      <td className="py-2 px-4  border-gray-200">
+                        <div className="flex justify-center items-center gap-2">
+                          <Link href={`/admin/dashboard/faq/edit/${d.id}`}>
+                            <FaRegEdit className="text-2xl text-orange-500" />
+                          </Link>
+                          <p onClick={() => handleDelete(d.id)}>
+                            <FaRegTrashAlt className="text-2xl text-red-500" />
+                          </p>
+                        </div>
+                      </td>
+                      <td className="py-2 px-4 border-b border-gray-200">{d.title}</td>
+                      <td className="py-2 px-4 border-b border-gray-200">{d.description}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FaqPage;
