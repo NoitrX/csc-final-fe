1a0e02dc4109d330237dfd0a780d4f7fdb53c806
Naufhal <naufhalsimdigxrpl2@gmail.com>
2024-08-06 15:02:05 +0700

    EVENT CMS

---
+++ src/app/admin/dashboard/event/edit/[id]/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+import { NextPage } from "next";
+import InputLabel from "@/components/InputLabel";
+import XButton from "@/components/XButton";
+import axios from "axios";
+import { showSuccessAlert } from "@/utils/alert";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  params: { id: string };
+}
+
+const EditEvent: NextPage<Props> = ({ params }: { params: { id: string } }) => {
+  const router = useRouter();
+  const [form, setForm] = useState({
+    title_img: "",
+    description: "",
+    img_event: undefined,
+    dateEvent: "",
+  });
+  const getDataNow = async () => {
+    try {
+      const newRes = await axios.get(`http://localhost:9000/api/csc/event/${params.id}`);
+      setForm(newRes.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const [alert, setAlert] = useState({
+    status: false,
+    type: "",
+    msg: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = e.target;
+    if (files) {
+      setForm({ ...form, [name]: files[0] });
+    } else {
+      setForm({ ...form, [name]: value });
+    }
+  };
+
+  useEffect(() => {
+    getDataNow();
+  }, []);
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    try {
+      await axios.put("http://localhost:9000/api/csc/event/update/" + params.id, form, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      showSuccessAlert("Data event Has Been Updated Successfully!");
+      router.push("/admin/dashboard/event");
+    } catch (err: any) {
+      console.log(err);
+      setAlert({
+        status: true,
+        type: "error",
+        msg: err.response.data.msg,
+      });
+    }
+  };
+  return (
+    <div className=" max-w-screen">
+      <div className="border border-gray-200 p-6 mt-3 rounded-md w-full">
+        <h2 className="font-semibold">Form About</h2>
+        <p className="text-red-500">{alert.msg}</p>
+        <hr className="mt-2" />
+        <form className="w-full">
+          <div className="mt-3 flex w-full justify-between gap-3">
+            <InputLabel errors={alert.msg} label="Title Img" type="text" placeholder="Enter Title Image" name="title_img" value={form.title_img} onChange={handleChange} />
+          </div>
+
+          <div className="mt-3 flex w-full justify-between gap-3">
+            <InputLabel errors={alert.msg} label="Description" type="text" placeholder="Enter Description" name="description" value={form.description} onChange={handleChange} />
+            <InputLabel errors={alert.msg} label="Image" type="file" placeholder="Enter Image" name="img_event" onChange={handleChange} />
+          </div>
+
+          <div className="mt-3 flex w-full justify-between gap-3">
+            <InputLabel errors={alert.msg} label="Date Event" type="datetime-local" placeholder="Enter Date Event" name="dateEvent" value={form.dateEvent} onChange={handleChange} />
+          </div>
+
+          <div className="flex justify-end items-end mt-3">
+            <XButton onClick={handleSubmit}>SUBMIT</XButton>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default EditEvent;
--- src/app/admin/dashboard/event/page.tsx
+++ src/app/admin/dashboard/event/page.tsx
@@ -1,7 +1,92 @@
-import React from "react";
+"use client";
+import Thead from "@/components/Thead";
+import { FaRegEdit, FaRegTrashAlt } from "react-icons/fa";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { showConfirmDeleteAlert } from "@/utils/alert";
 
 function EventPage() {
-  return <div>EventPage</div>;
+  const [data, setData] = useState<EventData[]>([]);
+
+  interface EventData {
+    id: number;
+    img_event: string;
+    title_img: string;
+    description: string;
+    dateEvent: string;
+    createdAt: string;
+    updatedAt: string;
+  }
+
+  const getEventData = () => {
+    try {
+      fetch(`http://localhost:9000/api/csc/event`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setData(data.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async (id: number) => {
+    try {
+      await showConfirmDeleteAlert(`http://localhost:9000/api/csc/event/delete/${id}`);
+      await getEventData();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getEventData();
+  }, []);
+  return (
+    <div>
+      <div className="flex flex-col mt-6 text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl max-w-screen">
+        <div className="p-6">
+          <div className="flex flex-col justify-end items-end">
+            <button className="flex items-center py-2 px-16 bg-primary mb-4 text-white font-bold gap-5 font-ubuntu hover:bg-blue-700 transition-all duration-200">
+              <Link href={"/admin/dashboard/event/add"} className="uppercase">
+                Tambah DATA
+              </Link>
+            </button>
+          </div>
+
+          <div className="overflow-x-auto">
+            <table className="min-w-full bg-white border border-gray-200">
+              <Thead theads={["#", "Img Event", "Title Image", "Description", "Date Event", "Log"]} />
+              <tbody className="text-xs">
+                {data.map((d: EventData, i) => {
+                  return (
+                    <tr className="text-center" key={i}>
+                      <td className="py-2 px-4  border-gray-200">
+                        <div className="flex justify-center items-center gap-2">
+                          <Link href={`/admin/dashboard/event/edit/${d.id}`}>
+                            <FaRegEdit className="text-2xl text-orange-500" />
+                          </Link>
+                          <p onClick={() => handleDelete(d.id)}>
+                            <FaRegTrashAlt className="text-2xl text-red-500" />
+                          </p>
+                        </div>
+                      </td>
+                      <td className="py-2 px-4 border-b border-gray-200">{d.img_event ? <img src={`http://localhost:9000/${d.img_event}`} alt="" className="w-24" /> : "No Image"}</td>
+                      <td className="py-2 px-4 border-b border-gray-200">{d.description}</td>
+                      <td className="py-2 px-4 border-b border-gray-200">{d.title_img}</td>
+                      <td className="py-2 px-4 border-b border-gray-200">{d.dateEvent}</td>
+                      <td className="py-2 px-4 border-b border-gray-200">{d.createdAt}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default EventPage;
