1f210ff8bce981c9938e06b44157b6073511e842
Mednoob <ahmadyaqdhan123@gmail.com>
2024-08-09 00:08:37 +0900

    feat: public url

--- src/utils/apiUrls.ts
+++ src/utils/apiUrls.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.API_URL || "http://localhost:9000";
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:9000";
 export const BASE_API = `${BASE_URL}/api/csc`;
 export const API_ABOUT = `${BASE_API}/about`;
 export const API_EVENT = `${BASE_API}/event`;
